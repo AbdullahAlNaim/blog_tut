205c0f323d4888ed1452aa00f1ae7b100baa0e48
Bucker000 <aanaim96@gmail.com>
2023-06-30 14:40:56 -0400

    completed project changes

--- models/article.js
+++ models/article.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
-const marked = require('marked');
+const { marked } = require('marked');
 const slugify = require('slugify')
+
+
+//this is used to keep our markdown part protected from attacks
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
     title: {
@@ -22,12 +28,22 @@ const articleSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
+    },
+    sanitizedHTML: {
+        type: String,
+        required: true
     }
 })
 
 articleSchema.pre('validate', function (next) {
     if (this.title) {
         this.slug = slugify(this.title, { lower: true, strict: true })
+    }
+
+    if (this.markdown) {
+        //converts our markdown as html
+        //then purifies any code that could be in there
+        this.sanitizedHTML = dompurify.sanitize(marked(this.markdown));
     }
 
     next()
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
+
+//this is a library to be able to use method delete
 const methodOverride = require('method-override');
 const app = express();
 const port = 8080;
@@ -11,6 +13,7 @@ mongoose.connect('mongodb://127.0.0.1/blog');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }))
+//this is the line that lets us use override method within ejs
 app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
